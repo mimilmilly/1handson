b2fc4a95c2e2b6d084078ce98fabf7a495561daf
Milly <35267727+mimilmilly@users.noreply.github.com>
2025-06-08 20:11:00 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.getElementById("login-form").addEventListener("submit", function (e) {
-  e.preventDefault();
-  const email = e.target[0].value;
-  const password = e.target[1].value;
-
-  // ลองทำงานง่ายๆ (ตัวอย่าง)
-  if (email && password) {
-    alert(`Email: ${email}\nPassword: ${password}`);
-  } else {
-    alert("Please fill in all fields.");
-  }
-});
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+  
+    if (email && password) {
+      alert(`Email: ${email}\nPassword: ${password}`);
+    } else {
+      alert("Please fill in all fields.");
+    }
+  });
+  
