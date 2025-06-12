79b2561fba0a198d908c5027295106a377cfc209
Milly <35267727+mimilmilly@users.noreply.github.com>
2025-06-12 20:19:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,14 @@ document.getElementById("login-form").addEventListener("submit", function (e) {
   const email = document.getElementById("email").value.trim();
   const password = document.getElementById("password").value.trim();
   const emailError = document.getElementById("email-error");
-
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-if (!emailRegex.test(email)) {
-  emailError.style.visibility = "visible";
-  return;
-} else {
-  emailError.style.visibility = "hidden";
-}
+  if (!emailRegex.test(email)) {
+    emailError.style.display = "block"; 
+    return;
+  } else {
+    emailError.style.display = "none";
+  }
 
   if (password === "") {
     alert("Please enter your password.");
