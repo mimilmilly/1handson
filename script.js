3311b319a3367179ee85d32e6f542d3e5010fac3
Milly <35267727+mimilmilly@users.noreply.github.com>
2025-06-12 20:06:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 document.getElementById("login-form").addEventListener("submit", function (e) {
-    e.preventDefault();
-    const email = e.target[0].value;
-    const password = e.target[1].value;
-  
-    if (email && password) {
-      alert(`Email: ${email}\nPassword: ${password}`);
-    } else {
-      alert("Please fill in all fields.");
-    }
-  });
-  
+  e.preventDefault();
+  const email = e.target[0].value.trim();
+  const password = e.target[1].value.trim();
+
+  if (email && password) {
+    alert("Login Successful");
+  } else {
+    alert("Please fill in all fields.");
+  }
+});
